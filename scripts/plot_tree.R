4746730f4471e62cfb2bf0895ee4464859ba391a
Jason Stajich <jasonstajich.phd@gmail.com>
2023-11-26 14:05:49 -0800

    draw heatmap CNV

--- scripts/plot_tree.R
+++ scripts/plot_tree.R
@@ -9,12 +9,20 @@ library(tidytree)
 library(ggtreeExtra)
 library(ggstar)
 library(RColorBrewer)
-tree<-read.tree("strain_tree/RmucY2510_v1.All.SNP.fasttree_reroot.tre")
+tree1<-read.tree("strain_tree/RmucY2510_v1.All.SNP.fasttree_reroot.tre") %>% drop.tip(c("Ref_NRRLY2510"))
+
 #tree<-read.tree("strain_tree/Rmuc_v7.All.SNP.poppr.nj.tre")
 meta <- read_csv("strain_metdata.csv",col_names=TRUE) %>% 
-  mutate(label=Strain) %>% select(label,SimpleEnv,culture_collection) %>% 
+  select(Strain,SimpleEnv,culture_collection) %>% 
   filter(!is.na(SimpleEnv))
 
+CNV <- read_tsv("plots/strain_median_coverage.txt",col_names=TRUE, col_types = cols(.default = "?", chrlen = "i", CHR = "i"))
+strains = colnames(as.data.frame(CNV %>% select(-c("CHR", "chrlen"))))
+chroms <- CNV$CHR
+tCNV <- t(as.data.frame(CNV %>% select(-c("CHR", "chrlen"))))
+colnames(tCNV) <- chroms
+
+tree <- tree1 %>% left_join(meta,by = c("label"="Strain"))
 unique(meta$SimpleEnv)
 unique(meta$culture_collection)
 p <- ggtree(tree, layout="circular", size=0.3, branch.length = "none")
@@ -24,28 +32,16 @@ colourCount = length(unique(meta$SimpleEnv))
 getPalette = colorRampPalette(brewer.pal(9, "Set1"))
 
 p2<-p + geom_fruit(
-  data=meta,
   geom=geom_star,
   mapping=aes(y=label, fill=SimpleEnv, starshape=culture_collection),
   starstroke=0.2
 ) +   scale_fill_manual(values = getPalette(colourCount))
 
-ggsave("phylogram.pdf",p2)
+ggsave("cladogram.pdf",p2,width=10,height=10)
 
-p <- ggtree(tree, size=0.5,layout="circular") +
-    geom_tippoint(aes(color=SimpleEnv), 
-                size=1.5,
-                show.legend=FALSE) +
+
+p <- ggtree(tree, size=0.5) + geom_tiplab(hjust=-0.5,size=1.5) +
+  geom_tippoint(aes(color=SimpleEnv), size=1) +
   scale_fill_manual(values = getPalette(colourCount))
-p
-
-p2<-p + geom_fruit(
-  data=meta,
-  geom=geom_star,
-  mapping=aes(y=label, fill=SimpleEnv, group=culture_collection),
-  outlier.size=0.5,
-  outlier.stroke=0.08,
-  outlier.shape=21,
-  starstroke=0.2
-) +   scale_fill_manual(values = getPalette(colourCount))
-  
+p2 <- gheatmap(p,tCNV,colnames_offset_y=-0.5,font.size = 2,offset=0.5) + scale_fill_continuous(type = "viridis",name="Chrom\nCNV")
+ggsave("strain_tree/strain_tree_CNV.pdf",p2,width=10,height=20)
