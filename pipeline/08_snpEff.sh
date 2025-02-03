97d63bd502f7cc89762f696ad2981a6449dd5822
Jason Stajich <jasonstajich.phd@gmail.com>
2025-02-03 09:43:52 -0800

    no interpro overlap right now

--- pipeline/08_snpEff.sh
+++ pipeline/08_snpEff.sh
@@ -11,7 +11,6 @@ if [ -z $CPU ]; then
 fi
 MEM=64g
 TOPFOLDER=`pwd` # expecting to be run in top folder of the github checkout
-DOMAINS=$TOPFOLDER/genome/$(basename ${GFFGENOME} .gff)_InterproDomains.txt
 
 # this module defines SNPEFFJAR and SNPEFFDIR
 if [ -f config.txt ]; then
